3f42287fbcfe2900e0acab4a8b35f93830231035
Nenad Shkurtevski <nenad.shkurtevski1@gmail.com>
2023-09-25 12:54:25 +0200

    Projects Section added

---
+++ src/components/Projects.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Projects = () => {
+  return (
+    <div className="projects">
+      <Container>
+        <div className="projects-wrapper">
+          <h2>Projects</h2>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Projects;
--- src/components/container/AppWrapper.tsx
+++ src/components/container/AppWrapper.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import Banner from "../Banner";
 import Navbar from "../Navbar";
 import Skills from "../Skills";
+import Projects from "../Projects";
 
 const AppWrapper = () => {
   return (
     <React.Fragment>
       <Navbar />
       <Banner />
-      <Skills/>
+      <Skills />
+      <Projects />
     </React.Fragment>
   );
 };
