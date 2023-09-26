3249c7c1a5be7d74091d764d4df86152ba32ed82
Nenad Shkurtevski <nenad.shkurtevski1@gmail.com>
2023-09-26 16:36:26 +0200

    Contact Form Added

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,10 +1,48 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Contact = () => {
   return (
-    <section className="contact" id="contact">
-      <Container>Contact</Container>
+    <section className="contact" id="connect">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12}>
+            <div>
+              <h2>Get In Touch</h2>
+              <form>
+                <input type="hidden" name="form-name" value="contact" />
+                <Row>
+                  <Col size={12} sm={6} className="px-1">
+                    <input type="text" placeholder="First Name" />
+                  </Col>
+                  <Col size={12} sm={6} className="px-1">
+                    <input type="text" placeholder="Last Name" />
+                  </Col>
+                  <Col size={12} sm={6} className="px-1">
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Email Address"
+                    />
+                  </Col>
+                  <Col size={12} sm={6} className="px-1">
+                    <input type="tel" placeholder="Phone No." />
+                  </Col>
+                  <Col size={12} className="px-1">
+                    <textarea
+                      rows={6}
+                      name="message"
+                      placeholder="Message"
+                    ></textarea>
+                    <button type="submit">
+                      <span>Send</span>
+                    </button>
+                  </Col>
+                </Row>
+              </form>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 };
